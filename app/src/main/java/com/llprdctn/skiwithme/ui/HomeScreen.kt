23f4ffb049bb12c84fd1c234a296c49f89113d1d
LuX1nAt0r PC <l.landsteiner@t-online.de>
2022-01-13 22:42:20 +0100

    Played a little bit with the chatscreens layout

--- app/src/main/java/com/llprdctn/skiwithme/ui/HomeScreen.kt
+++ app/src/main/java/com/llprdctn/skiwithme/ui/HomeScreen.kt
@@ -2,14 +2,15 @@ package com.llprdctn.skiwithme.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -45,6 +46,9 @@ fun HomeScreen() {
                     MaterialTheme.colors.primary,
                     CircleShape
                 )
+
+
+
         )
 
 
